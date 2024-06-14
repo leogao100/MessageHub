4fb70b2a2cf0edcfcbdead5f5d4e345e10debcd0
xixismile <gaoleiccie@gmail.com>
2024-06-14 23:51:12 +1000

    update client structure to Typescript

---
+++ client/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true, // 使用单引号
+  semi: true, // 在语句末尾加上分号
+  trailingComma: "all", // 在对象或数组的最后一个元素后面加上逗号
+  printWidth: 80, // 打印宽度,超过会自动换行
+  tabWidth: 2, // 缩进使用两个空格
+  arrowParens: "always", // 箭头函数的参数使用圆括号,无论参数个数
+  bracketSpacing: true, // 在对象左右添加空格 { foo: foo }
+  jsxBracketSameLine: false, // JSX 的 > 单独一行渲染
+  jsxSingleQuote: true, // JSX 使用单引号
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import React, { Component, Fragment } from "react";
+import { Routes, Switch, Redirect, withRouter, RouterComponentProps } from "react-router-dom";
+
+import Layout from "";
+import Backdrop from "";
+import Toolbar from "";
+import MainNavigation from "";
+import ErrorHandler from "";
+import FeedPage from "";
+import LoginPage from "";
+import SignupPage from "";
+import SinglePostPage from "";
+import "./App.css";
+
+// Interface defining the structure of authentication data
